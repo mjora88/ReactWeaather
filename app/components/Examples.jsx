9ecd519841a1ef60123f77467fe3043e3ddeff5a
Dj Mjora <djmjora@Djs-MacBook-Pro.local>
2018-03-22 21:29:06 +0200

    examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,19 +1,9 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//       <h3>Examples Component</h3>
-//       </div>
-//     );
-//   }
-// });
-
 var Examples = (props) => {
   return(
   <div>
-  <h3>Examples Component!</h3>
+  <h3>Examples </h3>
   <h3>Example demonstration </h3>
   </div>
   )
